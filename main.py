2e074e596d62026462b9cd1c36765706450f2f13
haval5454 <havalopp1@gmail.com>
2026-07-13 15:22:58 +0300

    Implement video cloning and sending bot functionality

---
+++ main.py
@@ -0,0 +1,38 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+import os
+
+TOKEN = "8885034983:AAFH8f3bC0_uUttokkQoly1Raa1jZCInLu0"
+TARGET_ID = 8734106005
+
+async def clone_and_send(update: Update, context: ContextTypes.DEFAULT_TYPE):
+msg = update.message
+
+if msg.video:  
+    await msg.reply_text("Downloading...")  
+
+    file = await msg.video.get_file()  
+    path = f"{msg.video.file_unique_id}.mp4"  
+
+    await file.download_to_drive(path)  
+
+    await msg.reply_text("Uploading...")  
+
+    with open(path, "rb") as f:  
+        await context.bot.send_document(  
+            chat_id=TARGET_ID,  
+            document=f  
+        )  
+
+    os.remove(path)  
+
+    await msg.reply_text("Sent to target ID")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(
+MessageHandler(filters.VIDEO, clone_and_send)
+)
+
+print("Bot running...")
+app.run_polling().
